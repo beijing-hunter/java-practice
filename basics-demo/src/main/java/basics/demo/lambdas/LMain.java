8b84914ce710ae29d2f55a8a7912fffa3ba8482f
gyl <gaoyl@cmt.com.cn>
2020-10-16 10:43:26 +0800

    stream

--- basics-demo/src/main/java/basics/demo/lambdas/LMain.java
+++ basics-demo/src/main/java/basics/demo/lambdas/LMain.java
@@ -1,8 +1,10 @@
 package basics.demo.lambdas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Lambda表达式可以看成是匿名内部类，使用Lambda表达式时，接口必须是函数式接口
@@ -39,5 +41,18 @@ public class LMain {
 		System.out.println(datas);
 
 		datas.forEach(System.out::println);// 方法引用，引用的接口必须是函数式接口
+
+		List<Integer> dts = Arrays.asList(125, 34, 90, 14);
+		Collections.sort(dts, (a, b) -> {
+			return a < b ? 1 : -1;
+		});
+
+		dts.forEach(System.out::println);
+
+		List<Integer> newDts = dts.stream().filter((d) -> {
+			return d > 30;
+		}).collect(Collectors.toList());
+		
+		newDts.forEach(System.out::println);
 	}
 }
