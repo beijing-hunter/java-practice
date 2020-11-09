bca6c581ab1914f5edce42fd9fb7118021332990
gyl <gaoyl@cmt.com.cn>
2020-11-09 16:55:24 +0800

    add

--- shard.datasource/src/main/java/shard/datasource/factorys/DataSourceTableRule.java
+++ shard.datasource/src/main/java/shard/datasource/factorys/DataSourceTableRule.java
@@ -1,9 +1,19 @@
 package shard.datasource.factorys;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.util.StringUtils;
+
+import com.alibaba.fastjson.JSON;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.util.TablesNamesFinder;
 
 /**
  * 数据源表规则
@@ -12,6 +22,8 @@ import org.springframework.util.StringUtils;
  *
  */
 public class DataSourceTableRule {
+
+	private String defaultDataSourceKey = "dataSource_db01";
 
 	private Map<String, List<String>> tableRule;
 
@@ -45,16 +57,82 @@ public class DataSourceTableRule {
 			return null;
 		}
 
-		for (String key : this.tableRule.keySet()) {
+		Statement statement = null;
 
-			List<String> tableNames = this.tableRule.get(key);
+		try {
+			statement = CCJSqlParserUtil.parse(sql);
 
-			for (String tableName : tableNames) {
+			// System.out.println(statement.toString());
+			Select selectStatement = (Select) statement;
+			TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
+			List<String> tables = tablesNamesFinder.getTableList(selectStatement);
 
-				if (sql.indexOf(tableName) >= 0) {
-					return key;
+			Map<String, String> tableAndDataSourceMap = new HashMap<String, String>();// key:taleName value:dataSource
+
+			for (String sqlTable : tables) {
+
+				boolean isSuccess = false;
+
+				for (String key : this.tableRule.keySet()) {
+
+					List<String> tableNames = this.tableRule.get(key);
+
+					for (String tableName : tableNames) {
+
+						if (sqlTable.equals(tableName)) {
+							tableAndDataSourceMap.put(sqlTable, key);
+							isSuccess = true;
+							break;
+						}
+					}
+
+					if (isSuccess) {
+						break;
+					}
+				}
+
+				if (isSuccess == false) {
+					tableAndDataSourceMap.put(sqlTable, defaultDataSourceKey);
+				}
+
+			}
+
+			if (tableAndDataSourceMap.size() == 0) {
+				return null;
+			}
+
+			if (tableAndDataSourceMap.size() == 1) {
+				String dbKey = tableAndDataSourceMap.values().iterator().next();
+				System.out.println(JSON.toJSONString(tables) + "," + dbKey);
+			}
+
+			Iterator<String> dataSources = tableAndDataSourceMap.values().iterator();
+			String currentDataSourceKey = null;
+			int dbSize = 0;
+
+			while (dataSources.hasNext()) {
+				String dataSourceKey = dataSources.next();
+
+				if (StringUtils.isEmpty(currentDataSourceKey)) {
+					dbSize++;
+					currentDataSourceKey = dataSourceKey;
+				} else {
+
+					if (!currentDataSourceKey.equals(dataSourceKey)) {
+						dbSize++;
+					}
 				}
 			}
+
+			if (dbSize == 1) {
+
+				System.out.println(JSON.toJSONString(tables) + "," + currentDataSourceKey);
+				return currentDataSourceKey;
+			}
+
+		} catch (JSQLParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		return null;
